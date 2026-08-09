e584625efd723577da9d2eba5680b907dce7268d
joshi <vedantjoshi335@gmail.com>
2026-08-09 08:03:51 +0530

    initial commmit

---
+++ src/main/java/com/project/Digital/Evidence/Verification/and/Storage/System/DigitalEvidenceVerificationAndStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.Digital.Evidence.Verification.and.Storage.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalEvidenceVerificationAndStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalEvidenceVerificationAndStorageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Digital/Evidence/Verification/and/Storage/System/DigitalEvidenceVerificationAndStorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Digital.Evidence.Verification.and.Storage.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalEvidenceVerificationAndStorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
