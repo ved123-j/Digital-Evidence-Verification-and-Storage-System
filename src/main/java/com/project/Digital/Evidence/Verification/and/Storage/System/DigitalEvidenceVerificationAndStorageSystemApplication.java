b599765d66061560ab4b8f10cd434c7aa31a4de9
joshi <vedantjoshi335@gmail.com>
2026-08-26 21:00:09 +0530

    initial commmit

--- src/main/java/com/project/Digital/Evidence/Verification/and/Storage/System/DigitalEvidenceVerificationAndStorageSystemApplication.java
+++ src/main/java/com/project/Digital/Evidence/Verification/and/Storage/System/DigitalEvidenceVerificationAndStorageSystemApplication.java
@@ -1,13 +1,13 @@
 package com.project.Digital.Evidence.Verification.and.Storage.System;
 
+import org.apache.poi.sl.usermodel.ObjectMetaData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DigitalEvidenceVerificationAndStorageSystemApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DigitalEvidenceVerificationAndStorageSystemApplication.class, args);
-	}
 
+		SpringApplication.run(ObjectMetaData.Application.class, args);
+	}
 }
