d3367e517c41d4a8140f084ccdf6bab8495bab54
joshi <vedantjoshi335@gmail.com>
2026-09-01 09:47:30 +0530

    initial commmit

--- src/main/java/services/ClamAVService.java
+++ src/main/java/services/ClamAVService.java
@@ -1,4 +1,3 @@
-//package com.evidence.system.service;
 package services;
 import fi.solita.clamav.ClamAVClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,23 +9,18 @@ import java.io.InputStream;
 @Service
 public class ClamAVService
 {
-
     @Value("${clamav.host}")
     private String clamavHost;
-
     @Value("${clamav.port}")
     private int clamavPort;
-
-    public boolean scanForMalware(MultipartFile file) throws Exception {
-        // Instantiate external library client using injected properties
+    public boolean scanForMalware(MultipartFile file) throws Exception
+    {
         ClamAVClient client = new ClamAVClient(clamavHost, clamavPort);
-
         byte[] reply;
-        try (InputStream is = file.getInputStream()) {
+        try (InputStream is = file.getInputStream())
+        {
             reply = client.scan(is);
         }
-
-        // Correct static method call from ClamAVClient library
         return ClamAVClient.isCleanReply(reply);
     }
 }
